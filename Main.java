4ee8dd5db9522d9f8dc537ddf5e70aaf98cbceb1
Artem <art4000@list.ru>
2025-02-25 12:26:13 +0600

    Перепись

--- Main.java
+++ Main.java
@@ -1,35 +1,42 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        List<Integer> positiveNumbers = new ArrayList<>();
-
-        // Отфильтруйте положительные числа
-        for (int num : intList) {
-            if (num > 0) {
-                positiveNumbers.add(num);
-            }
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
         }
 
-        List<Integer> evenNumbers = new ArrayList<>();
+        // 1. Найти количество несовершеннолетних (т.е. людей младше 18 лет).
+        long underageCount = persons.stream()
+                .filter(person -> person.getAge() < 18)
+                .count();
+        System.out.println("Количество несовершеннолетних: " + underageCount);
 
-        // Найдите среди этих положительных чисел четные
-        for (int num : positiveNumbers) {
-            if (num % 2 == 0) {
-                evenNumbers.add(num);
-            }
-        }
+        // 2. Получить список фамилий призывников (т.е. мужчин от 18 и до 27 лет).
+        List<String> conscriptFamilies = persons.stream()
+                .filter(person -> person.getSex() == Sex.MAN)
+                .filter(person -> person.getAge() >= 18 && person.getAge() <= 27)
+                .map(Person::getFamily)
+                .collect(Collectors.toList());
+        System.out.println("Список фамилий призывников: " + conscriptFamilies.size()); // Выводим размер списка, чтобы не засорять консоль
 
-        // Отсортируйте отфильтрованные числа в порядке возрастания
-        Collections.sort(evenNumbers);
-
-        // Выведите результат на экран
-        for (int num : evenNumbers) {
-            System.out.println(num);
-        }
+        // 3. Получить отсортированный по фамилии список потенциально работоспособных людей с высшим образованием в выборке
+        // (т.е. людей с высшим образованием от 18 до 60 лет для женщин и до 65 лет для мужчин).
+        List<Person> workablePeople = persons.stream()
+                .filter(person -> person.getEducation() == Education.HIGHER)
+                .filter(person -> person.getAge() >= 18 && ((person.getSex() == Sex.WOMAN && person.getAge() <= 60) || (person.getSex() == Sex.MAN && person.getAge() <= 65)))
+                .sorted(Comparator.comparing(Person::getFamily))
+                .collect(Collectors.toList());
+        System.out.println("Список работоспособных людей с высшим образованием: " + workablePeople.size()); // Выводим размер списка
     }
 }
